cef939c4be83902ef54a7593ff9f727d22433a58
Sashank <hello@sashank.wiki>
2025-04-03 23:54:28 +0530

    Create default_config.py

---
+++ steamtime/config/default_config.py
@@ -0,0 +1,14 @@
+"""
+Default configuration values for GabeGardener.
+"""
+
+DEFAULT_CONFIG = {
+    "show_timer": True,
+    "enable_game_rotation": False,
+    "rotation_interval": 3600,  # 1 hour in seconds
+    "language": "en",
+    "auto_update_check": True,
+    "dashboard_enabled": False,
+    "dashboard_port": 5000,
+    "accounts": []
+}
